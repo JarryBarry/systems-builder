f7725307586aa50e97995844846b7e1a89dbe532
Jarry DevSec <128482687+JarryBarry@users.noreply.github.com>
2026-05-26 10:31:23 +0200

    Update TaskManager.py

--- TaskManager.py
+++ TaskManager.py
@@ -1,3 +1,5 @@
+import json
+
 class Task:
     def __init__(self, title):
         self.title = title
@@ -9,6 +11,10 @@ class Task:
 class TaskManager:
     def __init__(self):
         self.tasks = []
+        try:
+            self.load_tasks()
+        except:
+            print("No tasks to load")
 
     def add_task(self, title):
         new_task = Task(title)
@@ -32,11 +38,33 @@ class TaskManager:
             else:
                 stats["pending"] += 1
         return stats
+    
+    def save_tasks(self):
+        export_list = []
+        for i in self.tasks:
+            export_list.append( {
+                "title" : i.title,
+                "is_done" : i.is_done
+            })
+        with open ("tasks.json", "w") as file:
+            json.dump(export_list, file)
         
+    def load_tasks(self):
+        with open("tasks.json", "r") as file:
+            saved_data = json.load(file)
+            for i in saved_data:
+                new_task = Task(i["title"])
+                if i["is_done"] == True: new_task.mark_done()
+                self.tasks.append(new_task)
+
+    def clear_tasks(self):
+        self.tasks = []
+        with open("tasks.json", "w") as file:
+            json.dump(self.tasks, file)
 
 manager = TaskManager()
 while True:
-    action = input("What would you like to do? (add/show/finish/stats/quit) \n")
+    action = input("What would you like to do? (add/show/finish/stats/save/clear/quit) \n")
     if action.lower() == "add":
         title = input("Enter your task\n")
         manager.add_task(title)
@@ -49,6 +77,10 @@ while True:
     elif action.lower() == "stats":
         stats = manager.get_stats()
         print (stats)
+    elif action.lower() == "save":
+        manager.save_tasks()
+    elif action.lower() == "clear":
+        manager.clear_tasks()
     elif action.lower() == "quit":
         break
     else:
